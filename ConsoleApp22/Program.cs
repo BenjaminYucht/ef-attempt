804b53291bd1d7e467300e138d82c2748902cfc9
benyuchtatgmailcom <47188246+benyuchtatgmailcom@users.noreply.github.com>
2019-06-03 11:10:37 +0300

    Add files via upload

---
+++ ConsoleApp22/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ORM ctx=new ORM())
+            {
+                foreach(var person in ctx.People)
+                {
+                    Console.WriteLine($"Person ID={person.PersonID}: First Name={person.FirstName}: Last Name={person.LastName}:");
+                }
+                
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
